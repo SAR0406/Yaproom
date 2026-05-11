725204cb701c4b7ed4d92d2e6ab976b081ca48de
Sarthak Upadhyay <sarthak040624@gmail.com>
2026-05-11 12:40:29 +0530

    Update security.ts

--- packages/server/src/security.ts
+++ packages/server/src/security.ts
@@ -1,6 +1,6 @@
 import { createCipheriv, createDecipheriv, randomBytes, scryptSync, timingSafeEqual } from 'node:crypto';
 import type { FastifyRequest } from 'fastify';
-import { config } from './config';
+import { config } from './config.ts';
 
 const ENCRYPTION_PREFIX = 'enc:v1';
 const SCRYPT_KEYLEN = 64;
